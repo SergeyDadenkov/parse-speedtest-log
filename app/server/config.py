75a336b321350e3c87763471779a972cbc041727
Sergey Dadenkov <83767359+SergeyDadenkov@users.noreply.github.com>
2021-06-03 16:26:44 +0500

    Delete config.py

--- app/server/config.py
+++
@@ -1,3 +0,0 @@
-PATH_SPEEDTEST_LOG = "E:/speedtest_log.txt"
-INSERT_SPEEDTEST = f"insert into spravka.dbo.speedtest_log(timestamp, download_bandwidth, upload_bandwidth, interface_internalIp, interface_externalIp, server_ip) " \
-                   "values('{0}', {1}, {2}, '{3}', '{4}', '{5}')"
